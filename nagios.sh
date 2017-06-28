720f2ec924cc7e70a8fd6bdbf6edcb471a214283
dehaandirk <j.d.de.haan@st.hanze.nl>
2017-06-28 15:29:22 +0200

    master

--- nagios.sh
+++ nagios.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# variablen
+NAGIOS_VERSION="4.2.0"
+NAGIOS_PLUGINGS="2.1.2"
+
+THEMP="/tmp/download"
+
+
 # Prerequisites
 sudo apt-get update
 sudo apt-get --yes install wget build-essential apache2 php apache2-mod-php7.0 php-gd libgd-dev sendmail unzip
@@ -13,9 +20,11 @@ sudo usermod -a -G nagios,nagcmd www-data
 
 # Download Nagios
 
-wget https://assets.nagios.com/downloads/nagioscore/releases/nagios-4.2.0.tar.gz
-tar -xzf nagios*.tar.gz
-cd nagios-4.2.0
+mkdir -p $TEMP
+cd $TEMP
+wget https://assets.nagios.com/downloads/nagioscore/releases/nagios-$NAGIOS_VERSION.tar.gz
+tar -xzf nagios-$NAGIOS_VERSION.tar.gz
+cd nagios-$NAGIOS_VERSION
 
 
 # Compile & install nagios
@@ -32,10 +41,10 @@ sudo cp -R contrib/eventhandlers/ /usr/local/nagios/libexec/
 sudo chown -R nagios:nagios /usr/local/nagios/libexec/eventhandlers
 
 # download Plugins
-cd ~
-wget https://nagios-plugins.org/download/nagios-plugins-2.1.2.tar.gz
-tar -xzf nagios-plugins*.tar.gz
-cd nagios-plugins-2.1.2/
+cd $TEMP
+wget https://nagios-plugins.org/download/nagios-plugins-$NAGIOS_PLUGINGS.tar.gz
+tar -xzf nagios-plugins-$NAGIOS_PLUGINGS.tar.gz
+cd nagios-plugins-$NAGIOS_PLUGINGS/
 
 # Install Plugins
 sudo ./configure --with-nagios-user=nagios --with-nagios-group=nagios --with-openssl
@@ -47,17 +56,17 @@ sudo mkdir -p /usr/local/nagios/etc/servers
 
 
 # Configuring Apache
-sudo a2enmod rewrite
-sudo a2enmod cgi
+#sudo a2enmod rewrite
+#sudo a2enmod cgi
 
-sudo htpasswd -c /usr/local/nagios/etc/htpasswd.users administrator
+#sudo htpasswd -c /usr/local/nagios/etc/htpasswd.users administrator
 
-sudo ln -s /etc/apache2/sites-available/nagios.conf /etc/apache2/sites-enabled/
+#sudo ln -s /etc/apache2/sites-available/nagios.conf /etc/apache2/sites-enabled/
 
 
-sudo service apache2 restart
+#sudo service apache2 restart
 
-sudo ln -s /etc/init.d/nagios /etc/rcS.d/S99nagios
+#sudo ln -s /etc/init.d/nagios /etc/rcS.d/S99nagios
 
 
 
