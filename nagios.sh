8fd3ee225ec4acead5503ca835704fc87bfd04f0
dehaandirk <j.d.de.haan@st.hanze.nl>
2017-06-28 15:38:08 +0200

    master

--- nagios.sh
+++ nagios.sh
@@ -56,17 +56,17 @@ sudo mkdir -p /usr/local/nagios/etc/servers
 
 
 # Configuring Apache
-#sudo a2enmod rewrite
-#sudo a2enmod cgi
+sudo a2enmod rewrite
+sudo a2enmod cgi
 
 #sudo htpasswd -c /usr/local/nagios/etc/htpasswd.users administrator
 
-#sudo ln -s /etc/apache2/sites-available/nagios.conf /etc/apache2/sites-enabled/
+sudo ln -s /etc/apache2/sites-available/nagios.conf /etc/apache2/sites-enabled/
 
 
-#sudo service apache2 restart
+sudo service apache2 restart
 
-#sudo ln -s /etc/init.d/nagios /etc/rcS.d/S99nagios
+sudo ln -s /etc/init.d/nagios /etc/rcS.d/S99nagios
 
 
 
