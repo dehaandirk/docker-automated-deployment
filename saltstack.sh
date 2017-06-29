5f06fde03a334758339f5d0ca1d64343c1196fda
dehaandirk <j.d.de.haan@st.hanze.nl>
2017-06-30 00:57:30 +0200

    master

--- saltstack.sh
+++ saltstack.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-SALT_MASTER_IP="192.168.2.143"
-
 sudo add-apt-repository ppa:saltstack/salt
 sudo apt-get update
 sudo apt-get install --yes salt-master salt-minion salt-ssh salt-cloud salt-doc salt-api salt-syndic
@@ -9,7 +7,7 @@ sudo apt-get install --yes salt-master salt-minion salt-ssh salt-cloud salt-doc 
 
 sudo service salt-master start
 
-sudo sed -i '/#master: salt/c\master: $SALT_MASTER_IP' /etc/salt/minion
+sudo sed -i '/#master: salt/c\master: 192.168.2.143' /etc/salt/minion
 
 (crontab -l 2>/dev/null; echo "*/5 * * * * sudo salt-key -Ay") | crontab -
 
